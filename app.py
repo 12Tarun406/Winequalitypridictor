555a3b71a4f80ce9022ae1650ccda39e3116c032
Tarun choudhary <tarun1940v@gmail.com>
2025-04-22 15:08:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ app = Flask(__name__)
 CORS(app)  # Allow React Native to access API
 
 # Load the trained model
-with open('model2.pkl', 'rb') as f:
-    model = pickle.load(f)
-
+import joblib
+model = joblib.load("model2.pkl")
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
@@ -20,6 +19,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Accessible from mobile
